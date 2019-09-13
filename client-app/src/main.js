a2ed551c455f91cd9d28e24f237062f7f125e9d6
Grin <grinefimov@yandex.ru>
2019-09-13 13:33:15 +0300

    Add BootstrapVue

--- client-app/src/main.js
+++ client-app/src/main.js
@@ -2,7 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import router from './router'
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
