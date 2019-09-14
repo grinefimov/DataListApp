d74aca586b5fb85d69ac75b51e1bc54214455e6b
Grin <grinefimov@yandex.ru>
2019-09-14 17:12:39 +0300

    Complete form validation

--- client-app/src/components/Form.vue
+++ client-app/src/components/Form.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="col-xl-4 offset-xl-4 col-md-6 offset-md-3">
     <b-card title="New Data">
-        <form @submit.prevent="createData">
+        <b-form @submit.prevent="createData" @change="creatingNewData">
           <b-form-group label="Data 1" description="Description">
-            <b-form-input v-model="model.data1" type="text" required placeholder="Enter data"></b-form-input>
+            <b-form-input v-model="model.data1" type="text" required placeholder="Enter data" :state="data1Validation"></b-form-input>
+            <b-form-invalid-feedback :state="data1Validation">
+        Data 1 must be 2-20 characters long.
+      </b-form-invalid-feedback>
+      <b-form-valid-feedback :state="data1Validation">
+        Looks Good.
+      </b-form-valid-feedback>
           </b-form-group>
           <b-form-group label="Data 2">
             <b-form-select v-model="model.data2" required>
+              <option :value="null" disabled>-- Please select an option --</option>
               <option value="First Option">First Option</option>
               <option value="Second Option">Second Option</option>
               <option value="Third Option">Third Option</option>
@@ -32,10 +39,11 @@
             </b-form-checkbox>
           </b-form-group>
           <div>
-            <b-btn type="submit" variant="success">Save Data</b-btn>
+            <b-btn type="submit" variant="primary">Save Data</b-btn>
           </div>
-        </form>
+        </b-form>
       </b-card>
+      <b-alert :show="dataSaved" variant="success" class="mt-2">Data saved</b-alert>
   </div>
 </template>
 
@@ -45,16 +53,37 @@ import api from '@/DataApiService';
   export default {
     data() {
       return {
-        model: {}
+        model: {
+          data1: '',
+          data2: null,
+          data5: "false"
+        },
+        dataSaved: false,
+        events: []
       };
+    },
+    computed: {
+      data1Validation() {
+        if(this.model.data1 == '') return null;
+        return this.model.data1.length >= 2 && this.model.data1.length <= 20
+      }
     },
     methods: {
       async createData() {
+        if(!this.data1Validation) return;
         
         await api.create(this.model)
 
-        // Clear the data inside of the form
-        this.model = {}
+        this.dataSaved = true;
+
+        this.model = {
+          data1: '',
+          data2: null,
+          data5: "false"
+        }
+      },
+      creatingNewData() {
+        this.dataSaved = false;
       }
     }
   }
