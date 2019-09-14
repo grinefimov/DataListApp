8e512087622e924e0a637783f5fb48d9cf1c760d
Grin <grinefimov@yandex.ru>
2019-09-14 23:12:45 +0300

    Add search and pagination
    * Add search
    * Add pagination
    * Add vuejs.org/.gitignore

--- client-app/src/components/Data.vue
+++ client-app/src/components/Data.vue
@@ -1,15 +1,48 @@
 <template>
-  <div class="container">
+  <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1">
     <h1 class="h1">Data list</h1>
-    <b-form-group>
-      <b-form-input type="text" placeholder="Search"></b-form-input>
-    </b-form-group>
+    <div class="row">
+      <div class="col-sm-6">
+        <b-form-group>
+          <b-form-input v-model="searchText" type="text" placeholder="Search"></b-form-input>
+        </b-form-group>
+      </div>
+      <div class="col-sm-2 mb-3">
+          <b-btn type="submit" variant="primary" v-on:click="searchItems">Search</b-btn>
+      </div>
+      <div class="col-sm-4 mb-3">
+        <b-form inline>
+          <label class="mr-2">Show:</label>
+          <b-form-select v-model="perPage">
+            <option value="10">10</option>
+            <option value="25">25</option>
+            <option value="50">50</option>
+          </b-form-select>
+        </b-form>
+      </div>
+    </div>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      aria-controls="my-table"
+    ></b-pagination>
     <b-alert :show="loading" variant="info">Loading...</b-alert>
       <b-table class="table-striped text-nowrap"
-        :items="dataItems"
+        :items="items"
         :fields="fields"
-        responsive>
+        :per-page="perPage"
+        :current-page="currentPage"
+        responsive
+        bordered
+        small>
       </b-table>
+      <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      aria-controls="my-table"
+    ></b-pagination>
   </div>
 </template>
 
@@ -28,11 +61,20 @@
           { key: 'data4', label: 'Data 4', sortable: true },
           { key: 'data5', label: 'Data 5', sortable: true }
         ],
+        items: [],
         dataItems: [],
+        perPage: 25,
+        currentPage: 1,
+        searchText: ''
       };
     },
     async created() {
-      this.getAll()
+      this.getAll();
+    },
+    computed: {
+      rows() {
+        return this.items.length
+      }
     },
     methods: {
       async getAll() {
@@ -41,8 +83,23 @@
         try {
           this.dataItems = await api.getAll()
         } finally {
+          this.items = this.dataItems;
           this.loading = false
         }
+      },
+      searchItems() {
+        if(this.searchText == '') {
+          this.items = this.dataItems;
+          return;
+        }
+
+        var result = [];
+
+        result = this.dataItems.filter(obj =>
+          Object.keys(obj).some(key =>
+            obj[key].toString().toLowerCase().includes(this.searchText.toLowerCase())));
+
+        this.items = result;
       }
     }
   }
