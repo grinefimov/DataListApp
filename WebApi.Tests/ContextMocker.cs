c4e18620d981c421f9515566ca9022ba9656cb5a
Grin <grinefimov@yandex.ru>
2019-09-17 11:33:05 +0300

    Add unit tests

---
+++ WebApi.Tests/ContextMocker.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Tests
+{
+    public class ContextMocker
+    {
+        public static DataContext GetDataContext(string databaseName)
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: databaseName)
+                .Options;
+
+            var context = new DataContext(options);
+
+            context.Seed();
+
+            return context;
+        }
+    }
+}
